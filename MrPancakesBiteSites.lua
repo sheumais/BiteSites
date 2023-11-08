74033889a9f357d43c1acc0b3412ef86e1867420
sheumais <88064574+sheumais@users.noreply.github.com>
2023-11-08 22:12:55 +1100

    Rename Addon

--- MrPancakesBiteSites.lua
+++ MrPancakesBiteSites.lua
@@ -1,13 +1,13 @@
 local LAM = LibAddonMenu2
 local LMP = LibMapPins
 
-local addonName = "CyrodiilVampires"
-local PIN = "CyrodiilVampiresPIN"
+local addonName = "MrPancakesBiteSites"
+local PIN = "MrPancakesBiteSitesPIN"
 
 local pinTextures = {
-    [1] = "CyrodiilVampires/Icons/fangs_white.dds",
-    [2] = "CyrodiilVampires/Icons/fangs_black.dds",
-    [3] = "CyrodiilVampires/Icons/imperial_camp.dds"
+    [1] = "MrPancakesBiteSites/Icons/fangs_white.dds",
+    [2] = "MrPancakesBiteSites/Icons/fangs_black.dds",
+    [3] = "MrPancakesBiteSites/Icons/imperial_camp.dds"
 }
 
 local pinTexturesList = {
@@ -18,9 +18,9 @@ local pinTexturesList = {
 
 local pinColor
 
-CyrodiilVampires = ZO_Object:Subclass()
-local T = CyrodiilVampires
-T.name = "CyrodiilVampires"
+MrPancakesBiteSites = ZO_Object:Subclass()
+local T = MrPancakesBiteSites
+T.name = "MrPancakesBiteSites"
 T.version = "1.0"
 T.debug = false
 T.UI = {}
@@ -75,302 +75,317 @@ local vampData = {
                 [2] = .198,
                 [3] = 1
             },
-            [8] = { -- house ava_whole_0={{.789,.273,140}},
+            [8] = { -- house {.789,.273,140},
                 [1] = .789,
                 [2] = .273,
                 [3] = 2
             },
-            [9] = { -- imperial camp ava_whole_0={{.786,.29,140}},
+            [9] = { -- imperial camp {.786,.29,140},
                 [1] = .786,
                 [2] = .290,
                 [3] = 1
             },
-            [10] = { -- imperial camp ava_whole_0={{.682,.287,140}},
+            [10] = { -- imperial camp {.682,.287,140},
                 [1] = .682,
                 [2] = .287,
                 [3] = 1
             },
-            [11] = { -- imperial camp ava_whole_0={{.649,.374,2778}},
+            [11] = { -- imperial camp {.649,.374,2778},
                 [1] = .649,
                 [2] = .374,
                 [3] = 1
             },
-            [12] = { -- imperial camp ava_whole_0={{.619,.408,2778}},
+            [12] = { -- imperial camp {.619,.408,2778},
                 [1] = .619,
                 [2] = .408,
                 [3] = 1
             },
-            [13] = { -- imperial camp ava_whole_0={{.573,.441,2778}},
+            [13] = { -- imperial camp {.573,.441,2778},
                 [1] = .573,
                 [2] = .441,
                 [3] = 1
             },
-            [14] = { -- imperial camp ava_whole_0={{.599,.356,2778}},
+            [14] = { -- imperial camp {.599,.356,2778},
                 [1] = .599,
                 [2] = .356,
                 [3] = 1
             },
-            [15] = { -- imperial camp ava_whole_0={{.61,.48,140}},
+            [15] = { -- imperial camp {.61,.48,140},
                 [1] = .610,
                 [2] = .480,
                 [3] = 1
             },
-            [16] = { -- black dagger camp ava_whole_0={{.639,.535,140}},
+            [16] = { -- black dagger camp {.639,.535,140},
                 [1] = .639,
                 [2] = .535,
                 [3] = 1
             },
-            [17] = { -- imperial camp ava_whole_0={{.721,.456,2778}},
+            [17] = { -- imperial camp {.721,.456,2778},
                 [1] = .721,
                 [2] = .456,
                 [3] = 1
             },
-            [18] = { -- imperial camp ava_whole_0={{.716,.417,2778}},
+            [18] = { -- imperial camp {.716,.417,2778},
                 [1] = .716,
                 [2] = .417,
                 [3] = 1
             },
-            [19] = { -- imperial camp ava_whole_0={{.786,.396,2778}},
+            [19] = { -- imperial camp {.786,.396,2778},
                 [1] = .786,
                 [2] = .396,
                 [3] = 1
             },
-            [20] = { -- imperial camp ava_whole_0={{.814,.5,140}},
+            [20] = { -- imperial camp {.814,.5,140},
                 [1] = .814,
                 [2] = .500,
                 [3] = 1
             },
-            [21] = { -- imperial camp ava_whole_0={{.852,.446,2778}},
+            [21] = { -- imperial camp {.852,.446,2778},
                 [1] = .852,
                 [2] = .446,
                 [3] = 1
             },
-            [22] = { -- imperial camp ava_whole_0={{.768,.64,140}},
+            [22] = { -- imperial camp {.768,.64,140},
                 [1] = .768,
                 [2] = .64,
                 [3] = 1
             },
-            [23] = { -- house ava_whole_0={{.715,.668,140}},
+            [23] = { -- house {.715,.668,140},
                 [1] = .715,
                 [2] = .668,
                 [3] = 2
             },
-            [24] = { -- imperial camp ava_whole_0={{.703,.708,2778}},
+            [24] = { -- imperial camp {.703,.708,2778},
                 [1] = .703,
                 [2] = .708,
                 [3] = 1
             },
-            [25] = { -- clearwater dock ava_whole_0={{.515,.822,2778}},
+            [25] = { -- clearwater dock {.515,.822,2778},
                 [1] = .515,
                 [2] = .822,
                 [3] = 3
             },
-            [26] = { -- imperial camp ava_whole_0={{.495,.801,140}},
+            [26] = { -- imperial camp {.495,.801,140},
                 [1] = .495,
                 [2] = .801,
                 [3] = 1
             },
-            [27] = { -- lowland camp ava_whole_0={{.481,.797,140}},
+            [27] = { -- lowland camp {.481,.797,140},
                 [1] = .481,
                 [2] = .797,
                 [3] = 1
             },
-            [28] = { -- imperial camp ava_whole_0={{.38,.74,140}},
+            [28] = { -- imperial camp {.38,.74,140},
                 [1] = .380,
                 [2] = .740,
                 [3] = 1
             },
-            [29] = { -- imperial camp ava_whole_0={{.328,.799,2778}},
+            [29] = { -- imperial camp {.328,.799,2778},
                 [1] = .328,
                 [2] = .799,
                 [3] = 1
             },
-            [30] = { -- nornalhorst ruins ava_whole_0={{.317,.71,140}},
+            [30] = { -- nornalhorst ruins {.317,.71,140},
                 [1] = .317,
                 [2] = .799,
                 [3] = 0
             },
-            [31] = { -- imperial camp ava_whole_0={{.277,.667,2778}},
+            [31] = { -- imperial camp {.277,.667,2778},
                 [1] = .277,
                 [2] = .667,
                 [3] = 1
             },
-            [32] = { -- gray viper camp ava_whole_0={{.461,.614,140}},
+            [32] = { -- gray viper camp {.461,.614,140},
                 [1] = .461,
                 [2] = .614,
                 [3] = 1
             },
-            [33] = { -- imperial camp ava_whole_0={{.255,.602,2778}},
+            [33] = { -- imperial camp {.255,.602,2778},
                 [1] = .255,
                 [2] = .602,
                 [3] = 1
             },
-            [34] = { -- imperial camp ava_whole_0={{.263,.527,2778}},
+            [34] = { -- imperial camp {.263,.527,2778},
                 [1] = .263,
                 [2] = .527,
                 [3] = 1
             },
-            [35] = { -- imperial camp ava_whole_0={{.29,.443,2778}},
+            [35] = { -- imperial camp {.29,.443,2778},
                 [1] = .290,
                 [2] = .443,
                 [3] = 1
             },
-            [36] = { -- imperial camp ava_whole_0={{.236,.308,140}},
+            [36] = { -- imperial camp {.236,.308,140},
                 [1] = .236,
                 [2] = .308,
                 [3] = 1
             },
-            [37] = { -- imperial camp ava_whole_0={{.32,.345,140}},
+            [37] = { -- imperial camp {.32,.345,140},
                 [1] = .320,
                 [2] = .345,
                 [3] = 1
             },
-            [38] = { -- black dagger camp ava_whole_0={{.463,.318,140}}, 
+            [38] = { -- black dagger camp {.463,.318,140}, 
                 [1] = .463,
                 [2] = .318,
                 [3] = 1
             },
-            [39] = { -- imperial camp ava_whole_0={{.527,.255,140}},
+            [39] = { -- imperial camp {.527,.255,140},
                 [1] = .527,
                 [2] = .255,
                 [3] = 1
             },
-            [40] = { -- tundra ravagers ava_whole_0={{.54,.174,2778}},
+            [40] = { -- tundra ravagers {.54,.174,2778},
                 [1] = .54,
                 [2] = .174,
                 [3] = 1
             },
-            [41] = { -- imperial camp ava_whole_0={{.489,.317,140}},
+            [41] = { -- imperial camp {.489,.317,140},
                 [1] = .489,
                 [2] = .317,
                 [3] = 1
             },
-            [42] = { -- imperial camp ava_whole_0={{.434,.341,140}},
+            [42] = { -- imperial camp {.434,.341,140},
                 [1] = .434,
                 [2] = .341,
                 [3] = 1
             },
-            [43] = { -- imperial camp ava_whole_0={{.389,.404,140}},
+            [43] = { -- imperial camp {.389,.404,140},
                 [1] = .389,
                 [2] = .404,
                 [3] = 1
             },
-            [44] = { -- imperial camp ava_whole_0={{.403,.485,2778}},
+            [44] = { -- imperial camp {.403,.485,2778},
                 [1] = .403,
                 [2] = .485,
                 [3] = 1
             },
-            [45] = { -- imperial camp ava_whole_0={{.442,.526,140}},
+            [45] = { -- imperial camp {.442,.526,140},
                 [1] = .442,
                 [2] = .526,
                 [3] = 1
             },
-            [46] = { --homestead camp ava_whole_0={{.479,.537,140}},
+            [46] = { --homestead camp {.479,.537,140},
                 [1] = .479,
                 [2] = .537,
                 [3] = 1
             },
-            [47] = { -- imperial camp ava_whole_0={{.506,.581,2778}},
+            [47] = { -- imperial camp {.506,.581,2778},
                 [1] = .506,
                 [2] = .581,
                 [3] = 1
             },
-            [48] = { -- sardavar ruins ava_whole_0={{.529,.577,140}},
+            [48] = { -- sardavar ruins {.529,.577,140},
                 [1] = .529,
                 [2] = .577,
                 [3] = 0
             },
-            [49] = { -- imperial camp ava_whole_0={{.612,.564,68}},
+            [49] = { -- imperial camp {.612,.564,68},
                 [1] = .612,
                 [2] = .564,
                 [3] = 1
             },
-            [50] = { -- river watch lighthouse ava_whole_0={{.582,.661,68}},
+            [50] = { -- river watch lighthouse {.582,.661,68},
                 [1] = .582,
                 [2] = .661,
                 [3] = 1
             },
-            [51] = { -- imperial camp ava_whole_0={{.405,.612,2778}},
+            [51] = { -- imperial camp {.405,.612,2778},
                 [1] = .407,
                 [2] = .612,
                 [3] = 1
             },
-            [52] = { -- imperial camp/shadowed path camp ava_whole_0={{.363,.695,68}},
+            [52] = { -- imperial camp/shadowed path camp {.363,.695,68},
                 [1] = .363,
                 [2] = .695,
                 [3] = 1
             },
-            [53] = { -- imperial camp ava_whole_0={{.365,.639,2778}},
+            [53] = { -- imperial camp {.365,.639,2778},
                 [1] = .365,
                 [2] = .639,
                 [3] = 1
             },
-            [54] = { -- imperial camp ava_whole_0={{.344,.573,68}},
+            [54] = { -- imperial camp {.344,.573,68},
                 [1] = .344,
                 [2] = .573,
                 [3] = 1
             },
-            [55] = { -- shadowed path camp ava_whole_0={{.317,.562,0}},
+            [55] = { -- shadowed path camp {.317,.562,0},
                 [1] = .317,
                 [2] = .562,
                 [3] = 1
             },
-            [56] = { -- black dagger village ava_whole_0={{.172,.476,140}},
+            [56] = { -- black dagger village {.172,.476,140},
                 [1] = .172,
                 [2] = .476,
                 [3] = 1
             },
-            [57] = { -- imperial camp ava_whole_0={{.195,.447,140}},
+            [57] = { -- imperial camp {.195,.447,140},
                 [1] = .195,
                 [2] = .447,
                 [3] = 1
             },
-            [58] = { -- imperial camp within a mountain ava_whole_0={{.206,.432,68}},
+            [58] = { -- imperial camp within a mountain {.206,.432,68},
                 [1] = .206,
                 [2] = .432,
                 [3] = 1
             },
-            [59] = { -- black dagger camp ava_whole_0={{.145,.401,140}},
+            [59] = { -- black dagger camp {.145,.401,140},
                 [1] = .145,
                 [2] = .401,
                 [3] = 1
             },
-            [60] = { -- black dagger house ava_whole_0={{.128,.385,140}},
+            [60] = { -- black dagger house {.128,.385,140},
                 [1] = .128,
                 [2] = .385,
                 [3] = 2
             },
-            [61] = { -- imperial camp ava_whole_0={{.116,.342,140}},
+            [61] = { -- imperial camp {.116,.342,140},
                 [1] = .116,
                 [2] = .342,
                 [3] = 1
             },
-            [62] = { -- bloodborne camp ava_whole_0={{.159,.244,68}},
+            [62] = { -- bloodborne camp {.159,.244,68},
                 [1] = .159,
                 [2] = .244,
                 [3] = 1
             },
-            [63] = { -- imperial camp ava_whole_0={{.174,.234,140}},
+            [63] = { -- imperial camp {.174,.234,140},
                 [1] = .174,
                 [2] = .234,
                 [3] = 1
             },
-            [64] = { -- imperial camp ava_whole_0={{.72,.582,140}},
+            [64] = { -- imperial camp {.72,.582,140},
                 [1] = .720,
                 [2] = .582,
+                [3] = 1
+            },
+            [65] = { -- bloodborn camp {.358,.225,68}, 
+                [1] = .358,
+                [2] = .225,
+                [3] = 1
+            },
+            [66] = { -- hearteater camp {.388,.205,0},
+                [1] = .388,
+                [2] = .205,
+                [3] = 1
+            },
+            [67] = { -- imperial camp {.589,.226,140},
+                [1] = .589,
+                [2] = .226,
                 [3] = 1
             }
         }
     }
 }
 
-local function CyrodiilVampires_GetLocalData(zone, subzone)
+local function MrPancakesBiteSites_GetLocalData(zone, subzone)
     if type(zone) == "string" and type(subzone) == "string" and vampData[zone] and vampData[zone][subzone] then
         return vampData[zone][subzone]
     end
 end
 
-local function CyrodiilVampires_SetLocalData(manifest, zone, subzone, data)
+local function MrPancakesBiteSites_SetLocalData(manifest, zone, subzone, data)
     if type(zone) == "string" and type(subzone) == "string" and type(data) == "table" then
         manifest[zone] = manifest[zone] or {}
         manifest[zone][subzone] = manifest[zone][subzone] or {}
@@ -383,8 +398,8 @@ end
 local function CreateSettingsMenu()
     local panelData = {
         type = "panel",
-        name = "Cyrodiil Vampires",
-        displayName = "|cFFAFAFCyrodiil Vampires|r",
+        name = "MrPancake's Bite Sites",
+        displayName = "|cFFAFAFMrPancake's Bite Sites|r",
         author = "@TheMrPancake",
         version = T.version,
         registerForRefresh = true,
@@ -395,7 +410,7 @@ local function CreateSettingsMenu()
 
 	local CreateIcons, icon
 	CreateIcons = function(panel)
-		if panel == CyrodiilVampires then
+		if panel == MrPancakesBiteSites then
 			icon = WINDOW_MANAGER:CreateControl(nil, panel.controlsToRefresh[2], CT_TEXTURE)
 			icon:SetAnchor(RIGHT, panel.controlsToRefresh[2].combobox, LEFT, -10, 0)
 			icon:SetTexture(pinTextures[savedVariables.pin.type])
@@ -485,7 +500,7 @@ local function PinCallback()
     if not IsPlayerActivated() then return end
 
     local zone, subzone = LMP:GetZoneAndSubzone()
-    local data = CyrodiilVampires_GetLocalData(zone, subzone)
+    local data = MrPancakesBiteSites_GetLocalData(zone, subzone)
 
     x,y = GetMapPlayerPosition("player")
     local campRange = savedVariables.pin.range / 100.0
@@ -518,7 +533,7 @@ end
 
 local function Init(event, name)
     if name ~= addonName then return end
-    savedVariables = ZO_SavedVars:NewAccountWide("CyrodiilVampires_SavedVariables", 1, nil, defaults)
+    savedVariables = ZO_SavedVars:NewAccountWide("MrPancakesBiteSites_SavedVariables", 1, nil, defaults)
 
     pinColor = ZO_ColorDef:New(savedVariables.pin.hex)
     local layout = {
